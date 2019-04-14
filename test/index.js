ed37f9258851ef8ba552926714f6094872c7edc4
Sho Hashimoto <hashimoto@shokai.org>
2019-04-14 22:53:08 +0900

    update tests

--- test/index.js
+++ test/index.js
@@ -7,4 +7,10 @@ describe('delay', () => {
   it('is function', () => {
     assert.isFunction(delay)
   })
+
+  it('wait msec', async () => {
+    const startAt = Date.now()
+    await delay(1000)
+    assert.isTrue(Date.now() - startAt > 1000)
+  })
 })
